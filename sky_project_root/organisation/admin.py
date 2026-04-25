36f8fc2fc944cb8e7b1746ddd9e670808c9ffa1c
Hana Selim <hana.nrhny@gmail.com>
2026-04-25 02:22:58 +0100

    Configured django admin interface and registered apps in settings

--- sky_project_root/organisation/admin.py
+++ sky_project_root/organisation/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TeamDependency, DependencyType
 
-# Register your models here.
+
+@admin.register(DependencyType)
+class DependencyTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(TeamDependency)
+class TeamDependencyAdmin(admin.ModelAdmin):
+    list_display = ("team", "depends_on", "dependency_type")
+    list_filter = ("dependency_type",)
+    search_fields = ("team__teamName", "depends_on__teamName")
--- sky_project_root/team/admin.py
+++ sky_project_root/team/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Department, Staff, Team, Skill, TeamSkill, TeamMember
 
-# Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("departmentID", "departmentName", "leader")
+    search_fields = ("departmentName",)
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ("staffID", "firstName", "lastName", "emailAddress", "department")
+    search_fields = ("firstName", "lastName", "emailAddress")
+    list_filter = ("department",)
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("teamID", "teamName", "department", "teamLeader")
+    search_fields = ("teamName",)
+    list_filter = ("department",)
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("skillID", "skillName", "skillCategory")
+    search_fields = ("skillName", "skillCategory")
+
+
+@admin.register(TeamSkill)
+class TeamSkillAdmin(admin.ModelAdmin):
+    list_display = ("team", "skill")
+    list_filter = ("team", "skill")
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ("staff", "team", "dateJoined")
+    list_filter = ("team",)
+    search_fields = ("staff__firstName", "staff__lastName")
