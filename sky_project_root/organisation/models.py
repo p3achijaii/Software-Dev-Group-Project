75e4fb0c24a737bc3a8cfd3e9f136bf3e7344a19
Hana Selim <hana.nrhny@gmail.com>
2026-04-25 01:28:48 +0100

    Implemented core models: Department, Staff, Team, SKill, TeamSkill, TeamDependency

--- sky_project_root/organisation/models.py
+++ sky_project_root/organisation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from team.models import Team
 
-# Create your models here.
+class TeamDependency(models.Model):
+    team = models.ForeignKey(Team, related_name='dependencies', on_delete=models.CASCADE)
+    depends_on = models.ForeignKey(Team, related_name='dependent_teams', on_delete=models.CASCADE)
+
+    dependencyType=models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"{self.team} depends on {self.depends_on}"
--- sky_project_root/team/models.py
+++ sky_project_root/team/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    departmentID = models.AutoField(primary_key=True)
+    departmentName = models.CharField(max_length=100)
+    deptHead = models.ForeignKey('Staff', on_delete=models.SET_NULL, null=True, blank=True, related_name='headed_departments')
+    def __str__(self):
+        return self.departmentName
+
+
+class Staff(models.Model):
+    staffID = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    emailAddress = models.EmailField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='staff_members')
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+
+class Team(models.Model):
+    teamID = models.AutoField(primary_key=True)
+    teamName = models.CharField(max_length=100)
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='teams')
+    teamLeader = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name='led_teams')
+
+    def __str__(self):
+        return self.teamName
+
+class Skill(models.Model):
+    skillID = models.AutoField(primary_key=True)
+    skillName = models.CharField(max_length=100)
+    skillCategory = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.skillName
+
+
+class TeamSkill(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.team} - {self.skill}"
