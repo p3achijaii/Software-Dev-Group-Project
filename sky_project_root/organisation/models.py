ed15c9f6f8f06a0393d2b2576a613a6e236512c9
Hana Selim <hana.nrhny@gmail.com>
2026-04-25 02:33:22 +0100

    Refined models metadata and added Dependency Type, and fixed some mismatching field names

--- sky_project_root/organisation/models.py
+++ sky_project_root/organisation/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from team.models import Team
 
+class DependencyType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Dependency Type"
+        verbose_name_plural = "Dependency Types"
+
 class TeamDependency(models.Model):
     team = models.ForeignKey(Team, related_name='dependencies', on_delete=models.CASCADE)
+
     depends_on = models.ForeignKey(Team, related_name='dependent_teams', on_delete=models.CASCADE)
 
-    dependencyType=models.CharField(max_length=50, blank=True)
+    dependency_type = models.ForeignKey(DependencyType, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.team} depends on {self.depends_on}"
+    
+    class Meta:
+        verbose_name = "Team Dependency"
+        verbose_name_plural = "Team Dependencies"
--- sky_project_root/team/models.py
+++ sky_project_root/team/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Department(models.Model):
     departmentID = models.AutoField(primary_key=True)
     departmentName = models.CharField(max_length=100)
-    deptHead = models.ForeignKey('Staff', on_delete=models.SET_NULL, null=True, blank=True, related_name='headed_departments')
+    leader = models.ForeignKey('Staff', on_delete=models.SET_NULL, null=True, blank=True, related_name='headed_departments')
     def __str__(self):
         return self.departmentName
 
@@ -17,6 +17,10 @@ class Staff(models.Model):
 
     def __str__(self):
         return f"{self.firstName} {self.lastName}"
+    
+    class Meta:
+        verbose_name = "Staff"
+        verbose_name_plural = "Staff"
 
 
 class Team(models.Model):
@@ -45,6 +49,18 @@ class TeamSkill(models.Model):
     def __str__(self):
         return f"{self.team} - {self.skill}"
 
+    class Meta:
+        verbose_name = "Team Skill"
+        verbose_name_plural = "Team Skills"
+
 class TeamMember(models.Model):
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    dateJoined = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.staff} in {self.team}"
+    
+    class Meta:
+        verbose_name = "Team Member"
+        verbose_name_plural = "Team Members"
