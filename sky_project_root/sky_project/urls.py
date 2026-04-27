5acaec93c1f467ce82c7303b6d5c119c226a317c
Jaina <jainamicah@gmail.com>
2026-04-27 14:58:08 +0100

    fix: update organisation navbar, format code, add homepage redirect to /graphs/, and delete unnecessary files

--- sky_project_root/sky_project/urls.py
+++ sky_project_root/sky_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from graphs.views import visualization_dashboard
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/graphs/', permanent=False)),  
+
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('organisation/', include('organisation.urls')),
