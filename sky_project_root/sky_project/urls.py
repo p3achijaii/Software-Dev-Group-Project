e2c216f314beca14fb64b7f0f09b1a2e93532d07
Jaina <jainamicah@gmail.com>
2026-04-27 17:16:53 +0100

    Remove home.html and core app after routing refactor

--- sky_project_root/sky_project/urls.py
+++ sky_project_root/sky_project/urls.py
@@ -18,12 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from graphs.views import visualization_dashboard
-from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', views.home, name='home'),  
+    path('', visualization_dashboard, name='home'),
     path('organisation/', include('organisation.urls')),
     path('dashboard/', visualization_dashboard, name='dashboard'),
 ]
