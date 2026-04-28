0bd440fe2b24f00cc19da365cd69add6798e3a97
Hana Selim <hana.nrhny@gmail.com>
2026-04-28 03:19:15 +0100

    fixed some errors

--- sky_project_root/organisation/admin.py
+++ sky_project_root/organisation/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import TeamDependency, DependencyType
 
-
+#Registers dependency models for admin interface
 @admin.register(DependencyType)
 class DependencyTypeAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
