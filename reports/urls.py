8c207266b3a74cefdacd2551ffa509fae0b075c7
Jaina <jainamicah@gmail.com>
2026-04-29 17:13:18 +0100

    fix: relocate reports folder to sky_project_root and resolve views.py syntax errors

--- reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.report_summary, name='report_summary'),
-    path('pdf/', views.export_report_pdf, name='export_report_pdf'),
-    path('excel/', views.export_report_excel, name='export_report_excel'),
-]
