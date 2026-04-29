852fb531ba908aaa48b2db925a3005c999082c62
KAWilliams <kennedyashleywilliams@gmail.com>
2026-04-29 13:29:37 +0100

    Update report URLs for summary and exports

--- reports/urls.py
+++ reports/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.report_summary, name='report_summary'),
-    path('', views.report_view, name='reports'),
-    path('download/pdf/', views.export_report_pdf, name='export_report_pdf'),
-    path('download/excel/', views.export_report_excel, name='export_report_excel'),
+    path('', views.report_summary, name='reports'),
+    path('pdf/', views.export_report_pdf, name='export_pdf'),
+    path('excel/', views.export_report_excel, name='export_excel'),
 ]
