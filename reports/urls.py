59012407e000a0b9f3beccee210a18fb2e334a83
KAWilliams <kennedyashleywilliams@gmail.com>
2026-04-29 13:09:11 +0100

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.report_summary, name='report_summary'),
+    path('', views.report_view, name='reports'),
     path('download/pdf/', views.export_report_pdf, name='export_report_pdf'),
     path('download/excel/', views.export_report_excel, name='export_report_excel'),
 ]
